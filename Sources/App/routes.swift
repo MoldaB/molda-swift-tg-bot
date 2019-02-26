d1684a1470440d9fde5b92a5f0139ee9efea17b7
Beygel <>
2019-02-26 10:05:38 +0200

    added empty route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,4 +2,7 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
+    router.get("/") { (request) -> String in
+        return "Welcome To Moldas Bot"
+    }
 }
